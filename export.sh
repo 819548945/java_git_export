f6d67517c5a2d267d185754d0143e2bf371d03ac
819548945 <30515245+819548945@users.noreply.github.com>
2025-04-17 12:08:08 +0800

    优化命令行展示效果

--- export.sh
+++ export.sh
@@ -3,6 +3,7 @@ ERROR_COLOR="\e[31m"
 SUCCESS_COLOR="\e[32m"
 INFO_COLOR="\e[34m"
 RES="\e[0m"
+
 createDir(){
     rm -rf $1;
     mkdir $1;
@@ -22,38 +23,40 @@ check(){
 }
 
 java(){ 
-  ##  createDir "$2/java";
-   ## output=$(pwd);
-    echo -e "${INFO_COLOR}导出java 文件${RES}";
+
+    echo -en "${INFO_COLOR}导出java 文件...${RES}";
     cd $1;
-    git diff --name-only --diff-filter=M "*.java" | xargs -I {} cp --parents {} $2;
-    
-  ##  cd $output;
+    git diff --name-only --diff-filter=M "*.java" > ${EXPORT_JAVA_FILE_LIST};
+    xargs -a ${EXPORT_JAVA_FILE_LIST} cp --parents -t $2;
+    echo -ne "\r";
+    echo -e "${SUCCESS_COLOR}导出java文件完成${RES}";
 }
 class(){
-   ## createDir "$2/class";
-    echo -e "${INFO_COLOR}导出class 文件${RES}";
+    echo -en "${INFO_COLOR}导出class 文件...${RES}";
     cd $1;
     mvn clean > ${LOG_FILE};
     mvn package >> ${LOG_FILE};
     git diff --name-only --diff-filter=M "*.java" > ${EXPORT_JAVA_FILE_LIST};
     sed 's/src\/main\/java/target\/classes/g; s/\.java/\.class/g' ${EXPORT_JAVA_FILE_LIST} > ${EXPORT_CLASS_FILE_LIST}
     xargs -a ${EXPORT_CLASS_FILE_LIST} cp --parents -t $2
+    echo -ne "\r";
+    echo -e "${SUCCESS_COLOR}导出class文件完成${RES}";
 }
-echo "lich git修改文件导出工具"
+set -e
+echo -e "\033[40;37m 欢迎使用lich java git修改文件导出工具${RES}"
 WORK_DIRECTORY=$2/export
 LOG_FILE=${WORK_DIRECTORY}/export.log
 EXPORT_JAVA_FILE_LIST=${WORK_DIRECTORY}/modified_java_files.txt
 EXPORT_CLASS_FILE_LIST=${WORK_DIRECTORY}/modified_class_files.txt
 if [ $# = 2 ];then
-  echo "工程目录 $1 导出目录$2";
+  echo -e "${INFO_COLOR}工程目录 $1 导出目录$2";
   if check $1 $2;then
      createDir ${WORK_DIRECTORY}
      java $1 ${WORK_DIRECTORY};
      class $1 ${WORK_DIRECTORY};
   fi
 elif [ $# = 3 ];then
-    echo "工程目录 $1 导出目录$2 类型$3";
+    echo -e "${INFO_COLOR}工程目录 $1 导出目录$2 类型$3${RES}";
     if check $1 $2;then
         createDir ${WORK_DIRECTORY}
         if [ $3 = 'java' ];then
@@ -61,7 +64,7 @@ elif [ $# = 3 ];then
         elif [ $3 = 'class' ];then
             class $1 ${WORK_DIRECTORY};
         else
-            echo "参数错误类型只允许java或class，当前为$3";
+            echo -e "${ERROR_COLOR}参数错误类型只允许java或class，当前为$3${RES}";
         fi
     fi
 else
